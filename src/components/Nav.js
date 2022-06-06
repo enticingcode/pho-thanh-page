ab76e767a6eeceb9547c42cedcda7062a2098544
Marvin <trujillomarvin@hotmail.com>
2022-06-05 22:24:41 -0500

    navbar work

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,9 @@ class Nav extends Component {
     render() {
         return (
             <nav className="nav">
-
+                <a href="" className="nav-btn">About</a>
+                <a href="" className="nav-btn">Menu</a>
+                <a href="" className="nav-btn">Contact</a>
             </nav>
         )
     }
