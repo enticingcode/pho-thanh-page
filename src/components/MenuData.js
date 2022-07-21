ddfbe8a8e88b354047b412823fa34d734c48f08f
Marvin <trujillomarvin@hotmail.com>
2022-07-21 15:17:02 -0500

    data update price

--- src/components/MenuData.js
+++ src/components/MenuData.js
@@ -3,95 +3,114 @@ export const appetizers = [
         id: "a1",
         name: "Goi cuon (Fresh Spring Roll)",
         description: "Shrimp, pork, lettuce, rice noodle paper w/ peanut sauce",
+        price: 4.50
     },
     {
         id: "a2",
         name: "Goi Cuon Nem Nuong(Fresh Special Spring roll) (2 Rolls)",
         description: "Pork patty, lettuce and rice noodle wrapped in rice pepper with peanut sauce",
+        price: 4.95
     },
     {
         id: "a3",
         name: "Cha Gio (Fried Egg Roll) (2 Rolls)",
         description: "Fried roll with ground pork, carrot, taro, cilantro, and onion",
+        price: 4.50
     },
 
     {
         id: "a4",
         name: "Canh Ga Chien (Chicken Wing) (6 Pieces)",
         description: "Spicy Chicken Wings",
+        price: 7.95
     },
     {
         id: "a5",
         name: "Crab Rangoon (4 Pieces)",
         description: "Crab rangoon puffs",
+        price: 5.50
     },
     {
         id: "a6",
         name: "Goi Cuon Thit Nuong(Grilled Pork Spring Roll)(2 Rolls)",
         description: "Grilled pork, lettuce and rice noodle wrapped in rice paper with fish sauce.",
+        price: 4.95
     },
     {
         id: "a7",
         name: "Goi Cuon Bi (Shredded Pork Spring Roll) (2 Rolls)",
         description: "Shredded pork, lettuce and rice paper wrapped in rice paper with fish sauce.",
+        price: 4.95
     },
     {
         id: "a8",
         name: "Sui Cao Hap Hoac Chien (8 Pieces)",
         description: "Steam or fried dumpling.",
+        price: 7.95
     },
 
 ]
+
 export const phoBowls = [
     {
         id: "p1",
         name: "Pho Dac Biet",
         description: "Eye round steak brisket, soft tendon, beef ball and tripe",
+        price: 10.50
     },
     {
         id: "p2",
         name: "Pho Tai",
         description: "Eye round steak",
+        price: 10.50
     },
     {
         id: "p3",
         name: "Pho Tai Nam",
         description: "Eye round steak and brisket",
+        price: 10.50
     },
     {
         id: "p4",
         name: "Pho Tai Bo Vien",
         description: "Eye round steak and beef ball",
+        price: 11.68
     },
     {
         id: "p5",
         name: "Pho Tai Gan",
         description: "Eye round steak brisket, soft tendon, beef ball and tripe",
+        price: 10.50
     },
     {
         id: "p6",
         name: "Pho Nam",
         description: "Brisket",
+        price: 10.50
     },
     {
         id: "p7",
         name: "Pho Bo Vien",
         description: "Beef ball",
+        price: 10.50
     },
     {
         id: "p8",
         name: "Pho Tai Nam Bo vien",
         description: "Eye round steak brisket and beef ball",
+        price: 10.50
     },
     {
         id: "p9",
         name: "Pho Do Bien",
         description: "Shrimp and crab stick",
+        price: 10.50
     },
     {
         id: "p10",
         name: "Pho Ga",
         description: "White meat chicken",
+        price: 11.68
     },
 ]
 
@@ -100,21 +119,25 @@ export const noodles = [
         id: "h1",
         name: "Huh Tieu",
         description: "Clear noodle",
+        price: 10.95
     },
     {
         id: "h2",
         name: "Mi",
-        description: "Egg noodle"
+        description: "Egg noodle",
+        price: 10.95
     },
     {
         id: "h3",
         name: "Hu Tieu Mi",
-        description: "Clear and egg noodle"
+        description: "Clear and egg noodle",
+        price: 11.50
     },
     {
         id: "h4",
         name: "Mi Thap Cam Hoanh Thanh",
-        description: "Egg noodle and wonton"
+        description: "Egg noodle and wonton",
+        price: 12.50
     }
 ]
 
@@ -122,46 +145,54 @@ export const sandwiches = [
     {
         id: "bm1",
         name: "Banh Mi Thit Nguoi",
-        description: "Special combination sandwich"
+        description: "Special combination sandwich",
+        price: 5.50
     },
     {
         id: "bm2",
         name: "Banh Mi Cha Lua",
-        description: "Vietnamese pork, ham sandwich"
+        description: "Vietnamese pork, ham sandwich",
+        price: 5.50
     },
     {
         id: "bm3",
         name: "Banh Mi Thit Nuong",
-        description: "Grilled pork sandwich. No mayonnaise and pate."
+        description: "Grilled pork sandwich. No mayonnaise and pate.",
+        price: 5.50
     },
 
     {
         id: "bm4",
         name: "Banh Mi Xa Xiu",
-        description: "Char sui sandwich."
+        description: "Char sui sandwich.",
+        price: 5.50
     },
 
     {
         id: "bm5",
         name: "Banh Mi Bi",
-        description: "Shredded pork sandwich. No mayonnaise and pate."
+        description: "Shredded pork sandwich. No mayonnaise and pate.",
+        price: 5.50
     },
     {
         id: "bm6",
         name: "Banh Mi Ga Nuong",
-        description: "Grilled chicken sandwich. No Mayonnaise and pate."
+        description: "Grilled chicken sandwich. No Mayonnaise and pate.",
+        price: 5.95
     },
 
     {
         id: "bm7",
         name: "Banh Mi Bo",
-        description: "Beef sandwich. No mayonnaise and pate."
+        description: "Beef sandwich. No mayonnaise and pate.",
+        price: 5.95
     },
 
     {
         id: "bm8",
         name: "Banh Mi Tofu",
-        description: "Tofu sandwich. No mayonnaise and pate."
+        description: "Tofu sandwich. No mayonnaise and pate.",
+        price: 5.50
     },
 ]
 
@@ -169,56 +200,66 @@ export const vermicelliNoodle = [
     {
         id: "b1",
         name: "Bun Thit Nuong",
-        description: "Grilled pork."
+        description: "Grilled pork.",
+        price: 9.95
     },
 
     {
         id: "b2",
         name: "Bun Thit Nuong Cha Gio",
-        description: "Grilled pork and egg rolls."
+        description: "Grilled pork and egg rolls.",
+        price: 10.50
     },
 
     {
         id: "b3",
         name: "Bun Bi Thit Nuong Cha Gio",
-        description: "Grilled pork, shredded pork and egg roll."
+        description: "Grilled pork, shredded pork and egg roll.",
+        price: 10.50
     },
 
     {
         id: "b4",
         name: "Bun Tom Nuong",
-        description: "Grilled shrimp"
+        description: "Grilled shrimp",
+        price: 10.95
     },
     {
         id: "b5",
         name: "Bun Tom Thit Nuong",
-        description: "Grilled pork and grilled shrimp."
+        description: "Grilled pork and grilled shrimp.",
+        price: 10.95
     },
     {
         id: "b6",
         name: "Bun Thap Cam Combination",
-        description: "Grilled pork, shrimp, shredded pork, and egg roll."
+        description: "Grilled pork, shrimp, shredded pork, and egg roll.",
+        price: 11.50
     },
 
     {
         id: "b7",
         name: "Bun Ga Nuong",
-        description: "Grilled white meat chicken"
+        description: "Grilled white meat chicken",
+        price: 10.95
     },
     {
         id: "b8",
         name: "Bun Bo Xao Sa",
-        description: "Spicy stir fried beef and lemongrass"
+        description: "Spicy stir fried beef and lemongrass",
+        price: 10.50
     },
     {
         id: "b9",
         name: "Bun Nem Nuong",
-        description: "Fried pork patty"
+        description: "Fried pork patty",
+        price: 12.50
     },
     {
         id: "b10",
         name: "Bun Ga Xao Sa",
-        description: "Spicy stir fried chicken and lemongrass"
+        description: "Spicy stir fried chicken and lemongrass",
+        price: 10.50
     },
 ]
 
@@ -227,19 +268,22 @@ export const stirFriedNoodle = [
     {
         id: "m1",
         name: "Mi Xao Gion",
-        description: "Crispy noodle. Stir-fried mixed vegetables and meat on the tap of crispy noodles."
+        description: "Crispy noodle. Stir-fried mixed vegetables and meat on the tap of crispy noodles.",
+        price: 10.95,
     },
 
     {
         id: "m2",
         name: "Mi Xao Mem",
-        description: "Soft egg noodle. Stir-fried mixed vegetables and meat with soft egg noodles."
+        description: "Soft egg noodle. Stir-fried mixed vegetables and meat with soft egg noodles.",
+        price: 10.95,
     },
 
     {
         id: "m3",
         name: "Pho Xao Ap Chao",
-        description: "Flat noodle. Stir-fried mixed vegetables and meat with flat noodles."
+        description: "Flat noodle. Stir-fried mixed vegetables and meat with flat noodles.",
+        price: 10.95,
     },
 ]
 
@@ -248,31 +292,37 @@ export const friedRice = [
     {
         id: "f1",
         name: "Chicken Fried Rice",
-        description: ""
+        description: "",
+        price: 9.95
     },
 
     {
         id: "f2",
         name: "Beef Fried Rice",
-        description: ""
+        description: "",
+        price: 9.95
+
     },
 
     {
         id: "f3",
         name: "Pork Fried Rice",
-        description: ""
+        description: "",
+        price: 9.95
     },
 
     {
         id: "f4",
         name: "Shrimp Fried Rice",
-        description: ""
+        description: "",
+        price: 10.95
     },
 
     {
         id: "f5",
         name: "Combination Fried Rice",
-        description: ""
+        description: "",
+        price: 10.95
     },
 ]
 
@@ -281,7 +331,7 @@ export const hotPot = [
     {
         id: "hp1",
         name: "Seafood Hot Pot with Pork Broth",
-        description: "Served with egg noodle."
+        description: "Served with egg noodle.",
     },
 
     {
@@ -303,92 +353,106 @@ export const comTamRiceDish = [
     {
         id: "c1",
         name: "Com Thit Nuong",
-        description: "Grilled pork with steamed rice."
+        description: "Grilled pork with steamed rice.",
+        price: 9.95
     },
     {
         id: "c2",
         name: "Com Bi Thit Nuong",
-        description: "Grilled pork and shredded pork with steam rice."
+        description: "Grilled pork and shredded pork with steam rice.",
+        price: 10.95
     },
     {
         id: "c3",
         name: "Com cha Thit Nuong",
-        description: "Grilled pork and pork cake with steam rice."
+        description: "Grilled pork and pork cake with steam rice.",
+        price: 10.95
     },
     {
         id: "c4",
         name: "Com bi Cha Thit Nuong",
-        description: "Grilled pork, pork cake and shredded pork."
+        description: "Grilled pork, pork cake and shredded pork.",
+        price: 12.50
     },
     {
         id: "c5",
         name: "Com Ga Nuong",
-        description: "Grilled white meat chicken with steam rice"
+        description: "Grilled white meat chicken with steam rice",
+        price: 10.95
     },
     {
         id: "c6",
         name: "Com Bo Luc Lac",
-        description: "Dice beef toss in garlic sauce steam rice."
+        description: "Dice beef toss in garlic sauce steam rice.",
+        price: 13.50
     },
     {
         id: "c7",
         name: "Com Suon bo",
-        description: "Korean BBQ short ribs (bone-in) with steam rice."
+        description: "Korean BBQ short ribs (bone-in) with steam rice.",
+        price: 13.50
     },
     {
         id: "c8",
         name: "Com Rau Xao",
-        description: "Stir fried brocolli and meat with steam rice."
+        description: "Stir fried brocolli and meat with steam rice.",
+        price: 10.95
     },
     {
         id: "c9",
         name: "Com Xao Bong Cai Xanh",
-        description: "Stir fried broccoli and meat with steam rice."
+        description: "Stir fried broccoli and meat with steam rice.",
+        price: 10.95
     },
 
 
 ]
 
 
-
-
 export const doChay = [
     {
         id: "v1",
         name: "Goi Cuon Tofu - Tofu Spring Roll",
-        description: "2 rolls. Mixed tofu and sweet potato, rice noodle, lettuce, and mint wrapped in rice paper with peanut sauce. "
+        description: "2 rolls. Mixed tofu and sweet potato, rice noodle, lettuce, and mint wrapped in rice paper with peanut sauce. ",
+        price: 3.95
     },
 
     {
         id: "v2",
         name: "Com Chien Rau Cai - Vegetables Fried Rice",
-        description: "Add tofu for an additional charge."
+        description: "Add tofu for an additional charge.",
+        price: 8.45
     },
     {
         id: "v3",
         name: "Lo Mein Tofu",
-        description: "Stir-fried mixed vegetable and tofu with lo-mein."
+        description: "Stir-fried mixed vegetable and tofu with lo-mein.",
+        price: 9.45
     },
     {
         id: "v4",
         name: "Com Xao Chay",
-        description: "Stir-fried mixed tofu and vegetable served with steamed rice."
+        description: "Stir-fried mixed tofu and vegetable served with steamed rice.",
+        price: 9.45
     },
     {
         id: "v5",
         name: "Bun Tofu",
-        description: "Spicy. Vermicelli noodle with stir-fried tofu and lemongrass."
+        description: "Spicy. Vermicelli noodle with stir-fried tofu and lemongrass.",
+        price: 9.45
     },
     {
         id: "v6",
         name: "Mi Xao Gion Chay",
-        description: "Stir-fried mixed vegetables and tofu on top of the crispy noodles."
+        description: "Stir-fried mixed vegetables and tofu on top of the crispy noodles.",
+        price: 9.45
     },
 
     {
         id: "v7",
         name: "Mi Xao Mem Chayy",
-        description: "Stir-fried mixed vegetable and tofu with soft egg noodle."
+        description: "Stir-fried mixed vegetable and tofu with soft egg noodle.",
+        price: 9.45
     },
 ]
 
@@ -396,23 +460,33 @@ export const loMein = [
     {
         id: "l1",
         name: "Pork Lo Mein",
-        description: ""
+        description: "",
+        price: 10.95
     },
     {
         id: "l2",
         name: "Beef Lo Mein",
-        description: ""
+        description: "",
+        price: 10.95
     },
     {
         id: "l3",
         name: "Chicken Lo Mein",
-        description: ""
+        description: "",
+        price: 10.95
     },
     {
         id: "l4",
         name: "Shrimp Lo Mein",
-        description: "Shrimp, chicken and beef."
+        description: "Shrimp, chicken and beef.",
+        price: 13.50
     },
+    {
+        id: "l5",
+        name: "Combination Lo Mein",
+        description: "Shrimp, chicken and beef.",
+        price: 13.50
+    }
 
 ]
 
@@ -420,47 +494,62 @@ export const kidsPho = [
     {
         id: "p2",
         name: "Kits Pho Tai",
-        description: "Eye round steak."
+        description: "Eye round steak.",
+        price: 7.95
     },
     {
         id: "p4",
         name: "Kits Pho tai Bo Vien",
-        description: "eye round steak and beef ball"
+        description: "eye round steak and beef ball",
+        price: 7.95
     },
     {
         id: "p5",
         name: "Kids Pho Tai Gan",
-        description: "Eye round steak and soft tendon"
+        description: "Eye round steak and soft tendon",
+        price: 7.95
     },
     {
         id: "p6",
         name: "Kids Pho Nam",
-        description: "Brisket"
+        description: "Brisket",
+        price: 11.68
+
     },
     {
         id: "p7",
         name: "Kids Pho Bo Vien",
-        description: "Beef ball."
+        description: "Beef ball.",
+        price: 7.95
+
     },
     {
         id: "p8",
         name: "Kids Pho Tai Nam Bo Vien",
-        description: "Eye round steak, brisket and beef ball."
+        description: "Eye round steak, brisket and beef ball.",
+        price: 7.95
+
     },
     {
         id: "p9",
         name: "Kids Pho Do Bien",
-        description: "Shrimp, squid and crab stick."
+        description: "Shrimp, squid and crab stick.",
+        price: 7.95
+
     },
     {
         id: "p10",
         name: "Kids Pho Ga",
-        description: "White meat chicken"
+        description: "White meat chicken",
+        price: 7.95
+
     },
     {
         id: "p11",
         name: "Kids Pho Rau CAi Va Tofu",
-        description: "Mixed vegetables and fried tofu."
+        description: "Mixed vegetables and fried tofu.",
+        price: 10.95
+
     },
 ]
 
@@ -468,32 +557,38 @@ export const thucuong = [
     {
         id: "soda",
         name: "Soda",
-        description: ""
+        description: "",
+        price: 1.95
     },
     {
         id: "icedTea",
         name: "Iced Tea",
-        description: ""
+        description: "",
+        price: 1.50
     },
     {
         id: "hotTea",
         name: "Hot Tea",
-        description: "Jasmine, Green tea"
+        description: "Jasmine, Green tea",
+        price: 1.50
     },
     {
         id: "soyMilk",
         name: "Soy Milk",
-        description: ""
+        description: "",
+        price: 1.50
     },
     {
         id: "cafeSuaDa",
         name: "Cafe Sua Da",
-        description: "Iced coffee with condensed milk."
+        description: "Iced coffee with condensed milk.",
+        price: 3.45
     },
     {
         id: "bubbleTea",
         name: "Bubble Tea",
-        description: "Green tea, avocado, strawberry, mango, coconut, lychee, peach, milk tea, Thai tea real bean, water, bananas, pineapple, almond."
+        description: "Green tea, avocado, strawberry, mango, coconut, lychee, peach, milk tea, Thai tea real bean, water, bananas, pineapple, almond.",
+        price: 3.95
     },
 ]
 
@@ -501,32 +596,38 @@ export const chefSpecial = [
     {
         id: "s1",
         name: "Bun Bo Hue",
-        description: "Rice noodle with beef shank, tender Vietnamese ham, pork, pork let in spicy beef soup."
+        description: "Rice noodle with beef shank, tender Vietnamese ham, pork, pork let in spicy beef soup.",
+        price: 10.95
     },
     {
         id: "s2",
         name: "Hu Tieu Bo Kho",
-        description: "Phe noodle with beef stew."
+        description: "Phe noodle with beef stew.",
+        price: 7.95
     },
     {
         id: "s3",
         name: "Mi Bo Kho",
-        description: "Egg noodle with beef stew."
+        description: "Egg noodle with beef stew.",
+        price: 7.95
     },
     {
         id: "s4",
         name: "Banh Mi Bo Kho",
-        description: "Beef stew with bread."
+        description: "Beef stew with bread.",
+        price: 7.95
     },
     {
         id: "s5",
         name: "Bo Luc Lac",
-        description: "Diced beef toss in garlic sauce served with steam rice."
+        description: "Diced beef toss in garlic sauce served with steam rice.",
+        price: 10.25
     },
     {
         id: "s6",
         name: "Korean BBQ",
-        description: "Short rib with stream rice."
+        description: "Short rib with stream rice.",
+        price: 8.95
     },
 
 ]
